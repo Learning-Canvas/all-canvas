31fac4c41db9f189d21769b14087ca1ce44110b1
VISH01AL <g.vish1al@gmail.com>
2023-04-17 13:32:05 +0530

    added first fractal

---
+++ 8-canvas-8.1-fractal-1/index.js
@@ -0,0 +1,17 @@
+let canvas=document.getElementById('canvas');
+let c=canvas.getContext('2d');
+canvas.width=innerWidth;
+canvas.height=innerHeight;
+function animate(x,y,radius){
+    if(radius>1){
+        c.beginPath();
+        c.arc(x,y,radius,0,Math.PI*2,false);
+        c.stroke()
+        animate(x+radius,y,radius/2)
+        animate(x-radius,y,radius/2)
+        animate(x,y+radius,radius/2)
+        
+        // animate(x,y,radius/2)
+    }   
+}
+animate(innerWidth/2,innerHeight/2,100)

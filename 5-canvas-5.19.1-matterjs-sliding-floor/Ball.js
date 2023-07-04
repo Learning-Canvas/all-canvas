e6099b2ff20cd54b30c1533725bb5d0d848558b2
VISH01AL <g.vish1al@gmail.com>
2023-07-04 23:10:02 +0530

    added deletion of bodies when out of screen

--- 5-canvas-5.19.1-matterjs-sliding-floor/Ball.js
+++ 5-canvas-5.19.1-matterjs-sliding-floor/Ball.js
@@ -7,6 +7,9 @@ export default class Ball{
         this.body=Bodies.circle(this.x,this.y,this.r,this.options)
         Composite.add(World, this.body);
     }
+    outsideofscreen(innerHeight,innerWidth){
+        return this.body.position.y>innerHeight+200 || this.body.position.x>innerWidth+200 || this.body.position.x<-200
+    }
     show(c){
         c.beginPath()
         c.arc(this.body.position.x,this.body.position.y,this.r,0,Math.PI*2,false)
--- 5-canvas-5.19.1-matterjs-sliding-floor/index.js
+++ 5-canvas-5.19.1-matterjs-sliding-floor/index.js
@@ -4,7 +4,7 @@ let canvas=document.getElementById("canvas")
 let c=canvas.getContext("2d")
 canvas.width=innerWidth
 canvas.height=innerHeight
-
+alert("click to create balls")
 let Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -16,10 +16,12 @@ let World=engine.world
 let runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
-
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 let balls=[]
-canvas.addEventListener("mousemove",(e)=>{
-    balls.push(new Ball(Bodies,Composite,World,e.clientX,e.clientY,Math.random()*30) )
+canvas.addEventListener("mousedown",(e)=>{
+    balls.push(new Ball(Bodies,Composite,World,e.clientX,e.clientY,randomIntFromInterval(10,30)) )
 })
 let floors=[]
 floors.push(new Floor(Bodies,Composite,World,innerWidth/4,innerHeight/2.3,innerWidth/2,10,0.2,0))
@@ -30,9 +32,14 @@ c.clearRect(0,0,innerWidth,innerHeight)
 requestAnimationFrame(animate)
 for(let i=0;i<balls.length;i++){
     balls[i].show(c)
+    if(balls[i].outsideofscreen(innerHeight,innerWidth)){
+        Composite.remove(World,balls[i].body)
+        balls.splice(i,1);
+    }
 }
 for(let i=0;i<floors.length;i++){
     floors[i].show(c)
 }
+// console.log(balls.length,Composite.allBodies(World)) //to check the size of the world
 }
 animate()

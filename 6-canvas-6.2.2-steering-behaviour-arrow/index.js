c84cd7bfa309a0bae1ee9f7dbb6a62f34336a098
VISH01AL <g.vish1al@gmail.com>
2023-07-05 22:13:46 +0530

    started triangle building

--- 6-canvas-6.2.2-steering-behaviour-arrow/index.js
+++ 6-canvas-6.2.2-steering-behaviour-arrow/index.js
@@ -1,5 +1,49 @@
+import { Pvector } from "./Pvector.js";
+
 let canvas=document.getElementById("canvas");
 let c=canvas.getContext("2d");
 canvas.width=innerWidth
 canvas.height=innerHeight
-
+class Agent{
+    constructor(){
+        this.location=new Pvector(0,0)
+        this.velocity=new Pvector(3,1)
+        this.acceleration=new Pvector(0,0)
+        this.maxvelocity=1
+        this.maxforce=0.1
+        this.radius=50
+    }
+    applyForce(force){
+        force.limit(this.maxforce)
+        this.acceleration.add(force)
+        this.velocity.add(this.acceleration)
+        this.velocity.limit(this.maxvelocity)
+        this.location.add(this.velocity)
+        this.acceleration.setmag(0)
+    }
+    update(){
+        this.velocity.add(this.acceleration)
+        this.velocity.limit(this.maxvelocity)
+        this.location.add(this.velocity)
+    }
+    display(c){
+        c.beginPath()
+        c.arc(this.location.x,this.location.y,this.radius,0,Math.PI*2,false)
+        c.stroke()    
+    }
+}
+let mouseposition=new Pvector(0,0)
+canvas.addEventListener("mousemove",(e)=>{
+    mouseposition.x=e.clientX
+    mouseposition.y=e.clientY
+})
+let a1=new Agent()
+function animate(){
+c.clearRect(0,0,innerWidth,innerHeight)
+requestAnimationFrame(animate)
+a1.update()
+let force=mouseposition.subvector(a1.location)
+a1.applyForce(force)
+a1.display(c)
+}
+animate()
